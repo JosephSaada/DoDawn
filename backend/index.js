0886f12187e1cd9fb8ce3dfbcb6e64d644ee849a
Mohammed Ezzaldiin <mezzaldi@uci.edu>
2024-05-23 15:58:06 -0700

    added mongo db + node js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-const app = express()
+mongoose.connect('mongodb://localhost:27017/DoDawn', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-app.listen(3000, ()=>{
+const userschema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
 
-    console.log("server is listening to 3000");
-})
+const usermodel = mongoose.model("students", userschema);
+
+app.get("/getusers", (req, res) => {
+  usermodel.find({})
+    .then(function(students) {
+      res.json(students); // Changed 'users' to 'students'
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).send("Error retrieving users");
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
